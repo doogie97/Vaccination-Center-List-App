e281e38a870d5aaebfeef153547f289212c47df6
doogie97 <chris_97@naver.com>
2022-09-27 16:28:40 +0900

    💎feat : ResponseResult 생성

---
+++ Vaccination-Center-List-App/Vaccination-Center-List-App/Utill/URLSessionProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  URLSessionProtocol.swift
+//  Vaccination-Center-List-App
+//
+//  Created by 최최성균 on 2022/09/27.
+//
+
+import Foundation
+
+struct ResponseResult {
+    let data: Data?
+    let response: URLResponse?
+    let error: Error?
+}
