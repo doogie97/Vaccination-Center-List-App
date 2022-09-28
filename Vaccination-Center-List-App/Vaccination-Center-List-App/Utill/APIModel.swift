40a06f40ef929b16aa57a713bbe8d06af0b5ef48
doogie97 <chris_97@naver.com>
2022-09-28 10:08:57 +0900

    💎feat : VaccinationAPIModel

--- Vaccination-Center-List-App/Vaccination-Center-List-App/Utill/APIModel.swift
+++ Vaccination-Center-List-App/Vaccination-Center-List-App/Utill/APIModel.swift
@@ -5,9 +5,25 @@
 //  Created by 최최성균 on 2022/09/27.
 //
 
+import Foundation
+
 protocol APIable {
     var host: String { get }
     var path: String { get }
     var params: [String: String]? { get }
     var method: HTTPMethod { get }
+}
+
+struct VaccinationAPIModel: APIable {
+    let host = "https://api.odcloud.kr/api"
+    let path = "/15077586/v1/centers"
+    let page: Int
+    let perPage: Int
+    let apiKey = Bundle.main.apiKey
+    var params: [String : String]? {
+        return ["page" : String(page),
+                "perPage" : String(perPage),
+                "serviceKey" : apiKey]
+    }
+    let method: HTTPMethod
 }
