9254919c4094fceab91124f99baa2d40506365e9
doogie97 <chris_97@naver.com>
2022-09-28 16:06:22 +0900

    💎feat : request메서드 구현

--- Vaccination-Center-List-App/Vaccination-Center-List-App/Utill/NetworkManager.swift
+++ Vaccination-Center-List-App/Vaccination-Center-List-App/Utill/NetworkManager.swift
@@ -14,6 +14,37 @@ struct NetworkManager {
         self.session = urlSession
     }
     
+    func request(api: APIable, completion: @escaping (Result<Data, APIError>) -> Void) {
+        guard let url = makeURL(api: api) else {
+            completion(.failure(APIError.urlError))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = api.method.string
+        
+        let dataTask = session.customDataTask(request: request) { responseResult in
+            guard responseResult.error == nil else {
+                completion(.failure(.transportError))
+                return
+            }
+            
+            guard let response = responseResult.response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+                completion(.failure(.responseError))
+                return
+            }
+            
+            guard let data = responseResult.data else {
+                completion(.failure(.dataError))
+                return
+            }
+            
+            completion(.success(data))
+        }
+        
+        dataTask.resume()
+    }
+    
     private func makeURL(api: APIable) -> URL? {
         var component = URLComponents(string: api.host + api.path)
         
