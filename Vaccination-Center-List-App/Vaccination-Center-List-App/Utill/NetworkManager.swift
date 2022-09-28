fd373f357261593354ed6f00dcaea291df2b5492
doogie97 <chris_97@naver.com>
2022-09-28 12:51:20 +0900

    💎feat : request 메서드 구현

--- Vaccination-Center-List-App/Vaccination-Center-List-App/Utill/NetworkManager.swift
+++ Vaccination-Center-List-App/Vaccination-Center-List-App/Utill/NetworkManager.swift
@@ -5,7 +5,7 @@
 //  Created by 최최성균 on 2022/09/28.
 //
 
-import Foundation
+import RxSwift
 
 struct NetworkManager {
     private let session: URLSessionProtocol
@@ -14,7 +14,43 @@ struct NetworkManager {
         self.session = urlSession
     }
     
-    private func makeURL(api: APIable) -> URL? {
+    func request(api: APIable) -> Observable<Data> {
+        return Observable.create { emitter in
+            guard let url = makeURL(api: api) else {
+                emitter.onError(APIError.urlError)
+                return Disposables.create()
+            }
+            
+            let dataTask = session.customDataTask(request: URLRequest(url: url)) { responseResult in
+                guard responseResult.error == nil else {
+                    emitter.onError(APIError.transportError)
+                    return
+                }
+                
+                guard let response = responseResult.response as? HTTPURLResponse,
+                      (200...299).contains(response.statusCode) else {
+                    emitter.onError(APIError.responseError)
+                    return
+                }
+                
+                guard let data = responseResult.data else {
+                    emitter.onError(APIError.dataError)
+                    return
+                }
+                
+                emitter.onNext(data)
+                emitter.onCompleted()
+            }
+            
+            dataTask.resume()
+            
+            return Disposables.create() {
+                dataTask.cancel()
+            }
+        }
+    }
+    
+    private func makeURL(api: APIable)-> URL? {
         var component = URLComponents(string: api.host + api.path)
         
         component?.percentEncodedQueryItems = api.params?.compactMap {
