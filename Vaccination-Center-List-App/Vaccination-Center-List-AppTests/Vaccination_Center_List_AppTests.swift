6d8bc5791a778b7c3090ea06471678ba61982c75
doogie97 <chris_97@naver.com>
2022-09-28 15:36:29 +0900

    🔬test : 테스트를 위한 makeDummyData 메서드 구현

--- Vaccination-Center-List-App/Vaccination-Center-List-AppTests/Vaccination_Center_List_AppTests.swift
+++ Vaccination-Center-List-App/Vaccination-Center-List-AppTests/Vaccination_Center_List_AppTests.swift
@@ -9,4 +9,11 @@ import XCTest
 @testable import Vaccination_Center_List_App
 
 final class Vaccination_Center_List_AppTests: XCTestCase {
+//MARK: NetworkManager 테스트
+    //테스트를 위한 메서드
+    func makeDummyData(data: Data?, statusCode: Int, error: Error?) -> ResponseResult {
+        let response = HTTPURLResponse(url: URL(string: "test")!, statusCode: statusCode, httpVersion: "2", headerFields: nil)
+        
+        return ResponseResult(data: data, response: response, error: error)
+    }
 }
