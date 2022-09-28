627bae117eaac459f33c991db866559839020ebe
doogie97 <chris_97@naver.com>
2022-09-28 12:57:38 +0900

    🔬test : 테스트 파일 생성

---
+++ Vaccination-Center-List-App/Vaccination-Center-List-AppTests/Vaccination_Center_List_AppTests.swift
@@ -0,0 +1,12 @@
+//
+//  Vaccination_Center_List_AppTests.swift
+//  Vaccination-Center-List-AppTests
+//
+//  Created by 최최성균 on 2022/09/28.
+//
+
+import XCTest
+@testable import Vaccination_Center_List_App
+
+final class Vaccination_Center_List_AppTests: XCTestCase {
+}
