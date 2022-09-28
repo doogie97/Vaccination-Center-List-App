d432e06df22e72d0a0ddfe92782e50be75f71d30
doogie97 <chris_97@naver.com>
2022-09-28 12:50:35 +0900

    💎feat : APIError 생성

---
+++ Vaccination-Center-List-App/Vaccination-Center-List-App/Utill/APIError.swift
@@ -0,0 +1,13 @@
+//
+//  APIError.swift
+//  Vaccination-Center-List-App
+//
+//  Created by 최최성균 on 2022/09/28.
+//
+
+enum APIError: Error {
+    case urlError
+    case transportError
+    case responseError
+    case dataError
+}
