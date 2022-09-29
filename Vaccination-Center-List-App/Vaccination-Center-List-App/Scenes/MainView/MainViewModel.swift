bd533c5721e5ee5eff1fa602d8332f86883a62f0
doogie97 <chris_97@naver.com>
2022-09-29 10:46:45 +0900

    💎feat : input getVaccinationsInfo 임시 구현

--- Vaccination-Center-List-App/Vaccination-Center-List-App/Scenes/MainView/MainViewModel.swift
+++ Vaccination-Center-List-App/Vaccination-Center-List-App/Scenes/MainView/MainViewModel.swift
@@ -9,7 +9,9 @@ import RxRelay
 
 protocol MainViewModelable: MainViewModelableInput, MainViewModelableOutput {}
 
-protocol MainViewModelableInput {}
+protocol MainViewModelableInput {
+    func getVaccinationsInfo()
+}
 
 protocol MainViewModelableOutput {
     var vaccinations: BehaviorRelay<[VaccinationInfo]> { get }
@@ -19,6 +21,25 @@ final class MainViewModel: MainViewModelable {
     private let networkManager = NetworkManager()
     private let decoder = DataDecoder()
     
+    func getVaccinationsInfo() {
+        let api = VaccinationAPIModel(page: 1, perPage: 10, method: .get)
+        networkManager.request(api: api) { [weak self] result in
+            switch result {
+            case .success(let data):
+                do {
+                    guard let vaccinations = try self?.decoder.parse(data: data, resultType: VaccinationsResult.self).data else {
+                        return
+                    }
+                    
+                    self?.vaccinations.accept(vaccinations)
+                } catch {
+                    
+                }
+            case .failure(let error):
+                print(error) //추후 에러처리 필요
+            }
+        }
+    }
     //out
     let vaccinations = BehaviorRelay<[VaccinationInfo]>(value: [])
 }
