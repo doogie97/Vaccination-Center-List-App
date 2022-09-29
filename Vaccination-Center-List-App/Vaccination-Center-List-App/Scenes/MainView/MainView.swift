16dd3e073c4c6763dd2b02dfc046969458877da4
doogie97 <chris_97@naver.com>
2022-09-29 10:45:47 +0900

    💎feat : MainView 구성

--- Vaccination-Center-List-App/Vaccination-Center-List-App/Scenes/MainView/MainView.swift
+++ Vaccination-Center-List-App/Vaccination-Center-List-App/Scenes/MainView/MainView.swift
@@ -10,9 +10,40 @@ import SnapKit
 final class MainView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private(set) lazy var listCollectionView: UICollectionView = {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(0.3))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: itemSize.widthDimension, heightDimension: itemSize.heightDimension)
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+
+        collectionView.register(VaccinationListCell.self, forCellWithReuseIdentifier: "\(VaccinationListCell.self)")
+        
+        collectionView.backgroundColor = .systemBackground
+        
+        return collectionView
+    }()
+    
+    private func setLayout() {
+        self.backgroundColor = .systemBackground
+        self.addSubview(listCollectionView)
+
+        listCollectionView.snp.makeConstraints {
+            $0.edges.equalTo(safeAreaLayoutGuide)
+        }
     }
 }
