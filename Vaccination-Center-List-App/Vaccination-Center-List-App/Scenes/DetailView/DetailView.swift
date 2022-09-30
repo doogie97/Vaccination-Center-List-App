34764b8ebc81799c71c8bdca9715b89b7182ed6b
doogie97 <chris_97@naver.com>
2022-09-30 11:30:22 +0900

    💎feat : descriptionLabel 생성 메서드 구현

--- Vaccination-Center-List-App/Vaccination-Center-List-App/Scenes/DetailView/DetailView.swift
+++ Vaccination-Center-List-App/Vaccination-Center-List-App/Scenes/DetailView/DetailView.swift
@@ -9,4 +9,11 @@ import SnapKit
 
 final class DetailView: UIView {
     
+    private func makeDescriptionLabel() -> UILabel {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .footnote)
+        return label
+    }
 }
